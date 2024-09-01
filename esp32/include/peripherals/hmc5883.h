dbc257dff30a3965b3aea87bfb16cfdb0621d28b
Rune Harlyk <rune@harlyk.dk>
2024-09-01 18:14:39 +0200

    🧭 Adds hmc5883 sensor

---
+++ esp32/include/peripherals/hmc5883.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <peripherals/sensor.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
+
+#ifdef USE_BMP
+class HMC5883Sensor : public Sensor {
+  private:
+    Adafruit_HMC5883_Unified mag;
+    sensors_event_t event;
+    bool is_active {false};
+    float heading;
+
+  public:
+    HMC5883Sensor() : mag(12345) {}
+
+    void initialize() override {
+        is_active = mag.begin();
+        if (!is_active) {
+            ESP_LOGE("HMC5883", "HMC5883 initialize failed");
+        }
+    }
+
+    void update() override {
+        mag.getEvent(&event);
+        heading = atan2(event.magnetic.y, event.magnetic.x);
+        float declinationAngle = 0.22;
+        heading += declinationAngle;
+        if (heading < 0) heading += 2 * PI;
+        if (heading > 2 * PI) heading -= 2 * PI;
+        heading *= 180 / M_PI;
+    }
+
+    const char* getName() const override { return "HMC5883"; }
+
+    float getHeading() const { return heading; }
+
+    void populateJson(JsonObject& root) override { root["heading"] = getHeading(); }
+
+    void printData() const {
+        Serial.print("Heading: ");
+        Serial.print(heading);
+    }
+};
+#endif
